b6ccde984684181c418673b8318b7124f0d47d63
Sergey Vasiliev <aragamerek@gmail.com>
2024-09-25 13:26:15 +0300

    feat: add user model

---
+++ UserRegistry/Models/User.cs
@@ -0,0 +1,10 @@
+namespace UserRegistry.Models;
+
+public class User
+{
+    public int Number { get; set; }
+    public string Identifier { get; set; }
+    public string FullName { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+}
